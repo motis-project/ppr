d8a06ce622d8b3a1ac9435866be72074efe333ad
Pablo Hoch <pablo@melaxis.com>
2022-07-25 21:58:24 +0200

    silence cista clang-tidy warnings

--- src/serialization/reader.cc
+++ src/serialization/reader.cc
@@ -9,15 +9,23 @@ namespace fs = boost::filesystem;
 
 namespace ppr::serialization {
 
+// NOLINTBEGIN(clang-analyzer-core.StackAddressEscape)
+
+// cista/reflection/to_tuple.h:133:5: error: Address of stack memory associated
+// with temporary object of type 'bool' is still referred to by a temporary
+// object on the stack upon returning to the caller.  This will be a dangling
+// reference
+
 void read_routing_graph(routing_graph& rg, std::string const& filename) {
   if (!fs::exists(filename)) {
     throw std::runtime_error{"ppr routing graph file not found"};
   }
   rg.data_buffer_ = cista::file(filename.c_str(), "r").content();
-  rg.data_ =
-      data::deserialize<routing_graph_data, SERIALIZATION_MODE>(  // NOLINT
-          rg.data_buffer_);  // NOLINT
+  rg.data_ = data::deserialize<routing_graph_data, SERIALIZATION_MODE>(
+      rg.data_buffer_);
   rg.filename_ = filename;
 }
+
+// NOLINTEND(clang-analyzer-core.StackAddressEscape)
 
 }  // namespace ppr::serialization
