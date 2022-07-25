48925e6abd00575549504eeb7ad5ccb90066e9b1
Pablo Hoch <pablo@melaxis.com>
2022-07-25 19:39:41 +0200

    ci: clang-tidy 14

--- cmake/clang-tidy.cmake
+++ cmake/clang-tidy.cmake
@@ -1,13 +1,13 @@
-if(CMake_SOURCE_DIR STREQUAL CMake_BINARY_DIR)
+if (CMake_SOURCE_DIR STREQUAL CMake_BINARY_DIR)
   message(FATAL_ERROR "CMake_RUN_CLANG_TIDY requires an out-of-source build!")
-endif()
+endif ()
 
 file(RELATIVE_PATH RELATIVE_SOURCE_DIR ${CMAKE_BINARY_DIR} ${CMAKE_SOURCE_DIR})
 
-find_program(CLANG_TIDY_COMMAND NAMES clang-tidy clang-tidy-11)
-if(NOT CLANG_TIDY_COMMAND)
+find_program(CLANG_TIDY_COMMAND NAMES clang-tidy clang-tidy-14)
+if (NOT CLANG_TIDY_COMMAND)
   message(FATAL_ERROR "CMake_RUN_CLANG_TIDY is ON but clang-tidy is not found!")
-endif()
+endif ()
 set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_COMMAND}")
 
 file(SHA1 ${CMAKE_CURRENT_SOURCE_DIR}/.clang-tidy.in clang_tidy_sha1)
