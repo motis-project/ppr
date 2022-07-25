abbaa8844a2a64877af43b054620cdfe1984d313
Pablo Hoch <pablohoch@users.noreply.github.com>
2022-07-25 22:44:37 +0200

    Update dependencies (boost 1.79, osmium 2.18...) (#22)

--- src/serialization/writer.cc
+++ src/serialization/writer.cc
@@ -7,6 +7,13 @@
 
 namespace ppr::serialization {
 
+// NOLINTBEGIN(clang-analyzer-core.StackAddressEscape)
+
+// cista/reflection/to_tuple.h:133:5: error: Address of stack memory associated
+// with temporary object of type 'bool' is still referred to by a temporary
+// object on the stack upon returning to the caller.  This will be a dangling
+// reference
+
 void write_routing_graph(routing_graph const& rg, std::string const& filename,
                          ppr::preprocessing::statistics& stats) {
 
@@ -14,5 +21,7 @@ void write_routing_graph(routing_graph const& rg, std::string const& filename,
   cista::serialize<SERIALIZATION_MODE>(mmap, *rg.data_);
   stats.serialized_size_ = static_cast<std::size_t>(mmap.size());
 }
+
+// NOLINTEND(clang-analyzer-core.StackAddressEscape)
 
 }  // namespace ppr::serialization
