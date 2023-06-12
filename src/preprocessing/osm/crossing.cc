6d410a0561d3e93997dcd3d2cafa0bc5f59f0994
Pablo Hoch <pablo@melaxis.com>
2023-06-12 13:02:22 +0200

    add basic support for the 2018/2019 osm crossing proposal
    see:
    https://wiki.openstreetmap.org/wiki/Crossings
    https://wiki.openstreetmap.org/wiki/Proposal:Crossing%3Dmarked
    https://wiki.openstreetmap.org/wiki/Proposal:Unambiguous_crossings

--- src/preprocessing/osm/crossing.cc
+++ src/preprocessing/osm/crossing.cc
@@ -7,9 +7,13 @@ namespace ppr::preprocessing::osm {
 crossing_type::crossing_type get_crossing_type(osmium::TagList const& tags) {
   auto const* crossing = tags["crossing"];
   if (crossing != nullptr) {
-    if (strcmp(crossing, "traffic_signals") == 0 ||
-        strcmp(crossing, "pelican") == 0 || strcmp(crossing, "toucan") == 0 ||
-        strcmp(crossing, "pegasus") == 0) {
+    if (strcmp(crossing, "marked") == 0) {
+      return tags.has_tag("crossing:signals", "yes") ? crossing_type::SIGNALS
+                                                     : crossing_type::MARKED;
+    } else if (strcmp(crossing, "traffic_signals") == 0 ||
+               strcmp(crossing, "pelican") == 0 ||
+               strcmp(crossing, "toucan") == 0 ||
+               strcmp(crossing, "pegasus") == 0) {
       return crossing_type::SIGNALS;
     } else if (strcmp(crossing, "uncontrolled") == 0 ||
                strcmp(crossing, "zebra") == 0 ||
@@ -21,7 +25,8 @@ crossing_type::crossing_type get_crossing_type(osmium::TagList const& tags) {
       return crossing_type::NONE;
     } else {
       // crossing=unmarked and unknown crossing types
-      return crossing_type::UNMARKED;
+      return tags.has_tag("crossing:signals", "yes") ? crossing_type::SIGNALS
+                                                     : crossing_type::UNMARKED;
     }
   } else {
     return tags.has_tag("highway", "crossing") ? crossing_type::UNMARKED
