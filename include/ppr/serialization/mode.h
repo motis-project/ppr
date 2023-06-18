644a9771d3bb78e2d68219e2c29ac7c5156ae141
Felix Gündling <felix.guendling@gmail.com>
2023-06-18 15:42:52 +0200

    unchecked graph deserialization for production mode
    for production purposes, integrity+version check are sufficient
    for development purposes, checks should be enabled

--- include/ppr/serialization/mode.h
+++ include/ppr/serialization/mode.h
@@ -4,10 +4,18 @@
 
 namespace ppr {
 
-constexpr auto const SERIALIZATION_MODE =
-    cista::mode::WITH_INTEGRITY | cista::mode::WITH_VERSION;
+constexpr auto const SERIALIZATION_MODE = cista::mode::WITH_INTEGRITY |
+                                          cista::mode::WITH_VERSION
+#ifndef NDEBUG
+                                          | cista::mode::UNCHECKED
+#endif
+    ;
 
 constexpr auto const SERIALIZATION_MODE_SKIP_INTEGRITY =
-    cista::mode::SKIP_INTEGRITY | cista::mode::WITH_VERSION;
+    cista::mode::SKIP_INTEGRITY | cista::mode::WITH_VERSION
+#ifndef NDEBUG
+    | cista::mode::UNCHECKED
+#endif
+    ;
 
 }  // namespace ppr
